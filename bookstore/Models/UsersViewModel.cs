95d48da37a4e08627fd44fe1374e1b7c50ca42b1
nevenaprtljaga <nevenaprtljaga@gmail.com>
2023-02-10 10:57:38 +0100

    Update Users changed, added StringOfRoles to ApplicationUserViewModel

--- bookstore/Models/UsersViewModel.cs
+++ bookstore/Models/UsersViewModel.cs
@@ -5,7 +5,7 @@ namespace bookstore.Models
     public class UsersViewModel
     {
         public ApplicationUser ApplicationUser { get; set; }
-        public string ListOfRoles { get; set; }
+        public string StringOfRoles { get; set; } = "Customer";
 
     }
 }
--- bookstore/Services/UsersService.cs
+++ bookstore/Services/UsersService.cs
@@ -33,7 +33,7 @@ namespace bookstore.Services
             {
                 await _userManager.AddToRolesAsync(user, new[] { role.Name });
             }
-            return new UsersViewModel { ApplicationUser = user };
+            return new UsersViewModel { ApplicationUser = user, StringOfRoles = role.Name };
         }
         public string randomPass(ApplicationUser ApplicationUser)
         {//username prvo veliko slovo + @ + prve tri cifre broja telefona   Nevenap@123
@@ -56,33 +56,21 @@ namespace bookstore.Services
             List<UsersViewModel> vm = new List<UsersViewModel>();
             var result = await _userManager.Users.ToListAsync();
 
-
             foreach (var item in result)
             {
                 var currentRoles = await _userManager.GetRolesAsync(item);
                 vm.Add(new UsersViewModel
                 {
                     ApplicationUser = item,
-                    ListOfRoles = string.Join(", ", currentRoles)
+                    StringOfRoles = string.Join(", ", currentRoles)
                 });
             }
-
-
-
             return vm;
         }
 
         public async Task<IEnumerable<Role>> GetAllRoles()
         {
-            List<RolesViewModel> vm = new List<RolesViewModel>();
             var result = await _context.Roles.ToListAsync();
-            /*   foreach (var item in result)
-               {
-                   vm.Add(new RolesViewModel//automapper ubaci
-                   {
-                       Role = item
-                   });
-               }*/
             return result;
         }
         public async Task<UpdateUserRolesViewModel> UpdateUserRoles(UpdateUserRolesViewModel vm)
@@ -101,7 +89,7 @@ namespace bookstore.Services
         public async Task<UsersViewModel> GetByIdAsync(string id)
         {
             var result = await _context.Users.FirstOrDefaultAsync(n => n.Id == id);
-            return new UsersViewModel { ApplicationUser = result };
+            return new UsersViewModel { ApplicationUser = result, StringOfRoles = "Customer" };
         }
 
         public async Task<UsersViewModel> UpdateAsync(string id, UsersViewModel usersViewModel)
